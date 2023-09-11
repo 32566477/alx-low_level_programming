e4db01f5db480439e8d0f070bede723cdb27463a
32566477 <paskalynnjeri@gmail.com>
2023-09-11 15:40:15 -0700

    4-puts.c  5-printf.c  6-size.c 2-assembler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main . Printing size in c program
+ * Return 0
+ */
+int main(void)
+{
+	char 1;
+	int 2;
+	long int 3;
+	long long int 4;
+	float 5;
+printf("size of char : % lu byte(s)\n *,(unsigned long)size of(1));
+printf("size of int : % lu byte(s)\n *,(unsigned long)size of(2));
+printf("size of long int : % lu byte(s)\n *,(unsigned long)size of(3)));
+printf("size of long long int : % lu byte(s)\n *,(unsigned long)size of(4));
+printf("size of float : % lu byte(s)\n *,(unsigned long)size of(5));
+return (0)
+}
